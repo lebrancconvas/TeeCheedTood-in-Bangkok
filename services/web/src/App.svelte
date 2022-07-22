8ecc62810106801b2b5c2c819cb2afe53ae97719
lebrancconvas <lebrancconvas@gmail.com>
2022-07-22 12:10:12 +0700

    ✨ feat: create routing.

--- services/web/src/App.svelte
+++ services/web/src/App.svelte
@@ -1,33 +1,18 @@
 <script lang="ts">
+	import Router from 'svelte-spa-router';
+	import Home from './routes/Home.svelte';
+	import Location from './routes/Location.svelte';
 
+	const routes = {
+		"/": Home,
+		"/location": Location
+	}
 </script>
 
 <main>
-	<div>
-		<h1>TeeCheedTood in Bangkok</h1>
-		<h2>Wanna find the public toilet with BumGun in Bangkok right?</h2>
-		<div class="find-buttom">Find TeeCheedTood</div>
-	</div>
+	<Router {routes}/>
 </main>
 
 <style>
-	.find-buttom {
-		background-color: #139dff;
-		color: #fff;
-		width: 30%;
-		padding: 10px;
-		border-radius: 5px;
-		text-align: center;
-		font-size: 20px;
-		margin: 20px auto;
-		cursor: pointer;
-	}
 
-	.find-buttom:hover {
-		background-color: rgb(30, 142, 239);
-	}
-
-	.find-buttom:active {
-		transform: scale(0.98);
-	}
 </style>
